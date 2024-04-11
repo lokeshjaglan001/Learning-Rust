e38dc9a498703d4f42a84d0ab7c8a497719469aa
Lokesh Jaglan <165664251+lokeshjaglan001@users.noreply.github.com>
2024-04-11 21:50:45 +0530

    day-1 code

---
+++ day-1.rs
@@ -0,0 +1,18 @@
+use std::io;
+use std::string::String;
+fn main(){
+// how to print something in rust
+  println!("Hello world !");
+// loop in rust
+  for i in 0..50{
+    println!("hii humanshu !");}
+  println!("Please enter the number : ");
+// how to take input from users
+  let mut num = String::new();
+  io::stdin().read_line(&mut num).expect("failed to read the number!");
+
+  let num: u32 = 
+  num.trim().parse().expect("Please enter a number!");
+    
+  println!("you enter {} ", num);
+}
